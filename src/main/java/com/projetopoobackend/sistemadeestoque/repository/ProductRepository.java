343db4d63dd6e5d7765b60b4a2df14089b013d98
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-05-20 11:18:04 -0300

    removing: productRepository methods

--- src/main/java/com/projetopoobackend/sistemadeestoque/repository/ProductRepository.java
+++ src/main/java/com/projetopoobackend/sistemadeestoque/repository/ProductRepository.java
@@ -1,8 +1,5 @@
 package com.projetopoobackend.sistemadeestoque.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +7,5 @@ import com.projetopoobackend.sistemadeestoque.domain.Product;
 
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
-
-    void save(List<Product> list);
-
-    Optional<Product> findById(String id);
 
 }
