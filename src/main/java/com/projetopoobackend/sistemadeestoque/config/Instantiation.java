2b0f7cebec6a99d0702d8a9ff657d18820a461b9
LucasHenrique-Silva <lucash.s.f74@gmail.com>
2024-05-27 15:32:19 -0300

    remove data delete & auto cadaster

--- src/main/java/com/projetopoobackend/sistemadeestoque/config/Instantiation.java
+++ src/main/java/com/projetopoobackend/sistemadeestoque/config/Instantiation.java
@@ -17,13 +17,7 @@ public class Instantiation implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        productRepository.deleteAll();
-
-        Product arroz = new Product(null, "Arroz", 10, "Alimentos", 12.5, 15.0, "Sala 2");
-        Product feijao = new Product(null, "Feijao", 10, "Alimentos", 15.0, 20.0, "Sala 1");
-
-        productRepository.save(arroz);
-        productRepository.save(feijao);
+      
 
     }
 
